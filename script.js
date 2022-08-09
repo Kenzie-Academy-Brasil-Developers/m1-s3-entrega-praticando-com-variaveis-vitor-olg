2fea0456138fcd62c1b144f5938564b0b3f8710a
olegario <voolegario2018@gmail.com>
2022-08-09 10:49:40 -0300

    versão principal

---
+++ script.js
@@ -0,0 +1,18 @@
+let nome = "Vitor"
+console.log(nome)
+
+let sobrenome = "Olegario"
+console.log(sobrenome) 
+
+let anoNascimento = 2000;
+let anoAtual = 2022;
+
+let idade = anoAtual - anoNascimento
+
+console.log (idade)
+
+let cidade = "capixaba"
+console.log(cidade)
+
+let templateInformation = `Eu sou ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade}`
+console.log(templateInformation)
